1cb52cc50248cf8ac2a2e4270147b9890d746da5
innovistar <45834254+innovistar@users.noreply.github.com>
2021-02-26 00:28:17 +0300

    Add files via upload

---
+++ backend/urls.py
@@ -0,0 +1,38 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+#from courses.views import course
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', course),
+    #path('accounts/', include('accounts.urls')),
+    path('auth/', include('users.urls'), name="auth"),
+    path('api/', include("courses.api.urls"), name="api"),
+    path('api/auth/', include('users.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('course/', include("courses.urls"), name="course"),
+    #path('profile/', include('user_profile.urls')),
+    
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
